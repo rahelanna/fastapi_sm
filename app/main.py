7fa150e76c7af2271e66267f112b8384a5538c5e
rahelanna <temesvari.rachel@gmail.com>
2024-08-17 20:44:17 +0300

    push to Render example

--- app/main.py
+++ app/main.py
@@ -27,4 +27,4 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello, welcome to my API! :)"}
+    return {"message": "Hello, welcome to my API! Deployed :)"}
