8ddf9550dd40b309ef2b0ef7caf988c0e0353c1a
rahelanna <temesvari.rachel@gmail.com>
2024-08-18 20:19:09 +0300

    push to Vercel

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
-from .routers import post, user, auth, vote
+from .routes import post, user, auth, vote
 
 
 
